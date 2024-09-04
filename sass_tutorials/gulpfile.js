d704e60d4b74b6477567d77be262087ee456b745
DJIOTSA CHRISTIAN (el Mhulo) <djiotsadjouakechristiandaryn@gmail.com>
2024-09-04 18:03:41 +0100

    customization using default

--- sass_tutorials/gulpfile.js
+++ sass_tutorials/gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const purgecss = require("gulp-purgecss");
 
 gulp.task('sass', function () {
-    return gulp.src('src/scss/**/*.scss')  // Source folder
+    return gulp.src('sass/**/*.scss')  // Source folder
         .pipe(sass().on('error', sass.logError))
         .pipe(purgecss({ content: ["*.html"]})) // this remove all the css code that is not used on our html files
         .pipe(gulp.dest('assets/css'));        // Destination folder
 });
 
 gulp.task('watch', function () {
-    gulp.watch(['src/scss/**/*.scss', '*.html'], gulp.series('sass')); // to make that gulp watch all the .scss and .html files
+    gulp.watch(['sass**/*.scss', '*.html'], gulp.series('sass')); // to make that gulp watch all the .scss and .html files
 });
 
 gulp.task('default', gulp.series('sass', 'watch'));
