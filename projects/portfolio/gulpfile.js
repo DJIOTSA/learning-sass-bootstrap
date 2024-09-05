dcd6de4fc00b3e6f8d9642694fa0a16cf98584a0
DJIOTSA CHRISTIAN (el Mhulo) <djiotsadjouakechristiandaryn@gmail.com>
2024-09-05 07:27:11 +0100

    update portfolio project

---
+++ projects/portfolio/gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require("gulp")
+const sass = require("gulp-sass")(require("sass"))
+// const purgecss = require("gulp-purgecss")
+
+gulp.task('sass', function () {
+    return gulp.src('scss/**/*.scss')  // Source folder
+        .pipe(sass().on('error', sass.logError))
+        // .pipe(purgecss({ content: ["*.html"]})) // this remove all the css code that is not used on our html files
+        .pipe(gulp.dest('assets/css'));        // Destination folder
+});
+
+gulp.task('watch', function () {
+    gulp.watch(['scss**/*.scss', '*.html'], gulp.series('sass')); // to make that gulp watch all the .scss and .html files
+});
+
+gulp.task('default', gulp.series('sass', 'watch'));
